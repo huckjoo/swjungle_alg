558442836e3e2e18641d1f892e2939a2d13e965d
huckjoo <huckjoo96@naver.com>
2021-11-26 21:12:41 +0900

    11053.py

---
+++ week04/11053.py
@@ -0,0 +1,12 @@
+import sys
+n = int(sys.stdin.readline())
+arr = list(map(int, sys.stdin.readline().split()))
+dp = [1]*n
+
+for i in range(1, n):
+    for j in range(n):
+        if j < i:
+            if arr[j] < arr[i]:
+                dp[i] = max(dp[i], dp[j]+1)
+
+print(max(dp))
