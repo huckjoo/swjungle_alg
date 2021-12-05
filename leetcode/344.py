8fd801fd40447dc4e847d44662784cf8827f5e6c
huckjoo <huckjoo96@naver.com>
2021-12-05 11:25:34 +0900

    334.py

---
+++ leetcode/344.py
@@ -0,0 +1,13 @@
+def reverseString(s):
+    N = len(s)
+    pl = 0
+    pr = N-1
+    while pl < pr:
+        s[pl], s[pr] = s[pr], s[pl]
+        pl += 1
+        pr -= 1
+    return s
+
+
+s = ["h", "e", "l", "l", "o"]
+print(reverseString(s))
---
+++ leetcode/344_1.py
@@ -0,0 +1,10 @@
+from collections import deque
+
+
+def reverseString(s):
+    s.reverse()
+    return s
+
+
+s = ["h", "e", "l", "l", "o"]
+print(reverseString(s))
