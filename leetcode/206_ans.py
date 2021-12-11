55bdfdc73e5d7b85df42fb5a8d871bf00c4b8c31
huckjoo <huckjoo96@naver.com>
2021-12-11 19:26:20 +0900

    2.py

--- leetcode/206_ans.py
+++ leetcode/206_ans.py
@@ -1,9 +1,13 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 # head만 가리키면 linked list 전체를 가리킬 수 있도록 세팅되어있는 것 같다.
+
+
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         prev = None
