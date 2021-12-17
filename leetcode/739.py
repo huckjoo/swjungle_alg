7c9dc9be3e953c5dddc09da0c0de3d820bb9c418
huckjoo <huckjoo96@naver.com>
2021-12-17 13:17:47 +0900

    739.py

---
+++ leetcode/739.py
@@ -0,0 +1,19 @@
+'''
+Runtime: 1497 ms, faster than 14.45% of Python3 online submissions for Daily Temperatures.
+Memory Usage: 25.3 MB, less than 68.77% of Python3 online submissions for Daily Temperatures.
+
+어떤 값을 스택에 넣고, 언제 빼줘야 하는지 감이 잘 안온다...
+'''
+
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        n = len(temperatures)
+        ans = [0]*n
+        stack = []
+        for cur_day, cur_temp in enumerate(temperatures):
+            while stack and cur_temp > temperatures[stack[-1]]:
+                prev_day = stack.pop()
+                ans[prev_day] = cur_day - prev_day
+            stack.append(cur_day)
+        return ans
