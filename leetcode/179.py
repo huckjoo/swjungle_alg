8e471e31a383139d64cccd8525bbf3c3fc0a4862
huckjoo <huckjoo96@naver.com>
2022-01-18 12:46:53 +0900

    179.py

---
+++ leetcode/179.py
@@ -0,0 +1,14 @@
+from typing import List
+def to_swap(n1,n2):
+    return str(n1)+str(n2) < str(n2)+str(n1)
+
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        i = 1
+        while i < len(nums):
+            j = i
+            while j > 0 and to_swap(nums[j-1],nums[j]):
+                nums[j],nums[j-1]=nums[j-1],nums[j]
+                j -= 1
+            i+=1
+        return str(int(''.join(map(str,nums))))
