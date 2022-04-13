cacb5e2d11e047294803daf630bf3ed0b29771d0
huckjoo <huckjoo96@naver.com>
2022-04-13 21:16:32 +0900

    learning js types

---
+++ test/types.js
@@ -0,0 +1,15 @@
+// 변수를 각각 선언하여 두 변수의 참조 값이 다른 상황.
+let a = [1, 2];
+let b = [1, 2];
+console.log(a === b); // false
+
+// 두 변수의 참조 값이 같은 상황.
+let c = [1, 2];
+let d = c;
+console.log(c === d); // true
+
+let e = [1, 2, 3];
+let f = e;
+f[0] = 8;
+console.log(f);
+console.log(e === f); // true
