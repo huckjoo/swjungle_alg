dca02496fa39c1fdb0d64127f2a13c8a5daf1c81
huckjoo <huckjoo96@naver.com>
2022-04-22 17:17:16 +0900

    프렌즈4블록.js

--- test/combination.js
+++ test/combination.js
@@ -1,34 +1,18 @@
-// function getCombinations(array, num) {
-//   let result = [];
-//   if (num === 1) {
-//     return array.map((e) => [e]);
-//   }
-//   array.forEach((fixed, idx, origin) => {
-//     let restCombinations = getCombinations(origin.slice(idx + 1), num - 1);
-//     let attached = restCombinations.map((restCombination) => [fixed, ...restCombination]);
-//     result.push(...attached);
-//   });
-//   return result;
-// }
+const getCombinations = function (arr, selectNumber) {
+  const results = [];
+  if (selectNumber === 1) return arr.map((value) => [value]); // 1개씩 택할 때, 바로 모든 배열의 원소 return
 
-// console.log(getCombinations([1, 2, 3], 2));
+  arr.forEach((fixed, index, origin) => {
+    const rest = origin.slice(index + 1); // 해당하는 fixed를 제외한 나머지 뒤
+    const combinations = getCombinations(rest, selectNumber - 1); // 나머지에 대해서 조합을 구한다.
+    const attached = combinations.map((combination) => [fixed, ...combination]); //  돌아온 조합에 떼 놓은(fixed) 값 붙이기
+    results.push(...attached); // 배열 spread syntax 로 모두다 push
+  });
 
-const relation = [
-  ['100', 'ryan', 'music', '2'],
-  ['200', 'apeach', 'math', '2'],
-  ['300', 'tube', 'computer', '3'],
-  ['400', 'con', 'computer', '4'],
-  ['500', 'muzi', 'music', '3'],
-  ['600', 'apeach', 'music', '2'],
-];
-
-attrIndexComb = [0, 1];
-
-const isUnique = (relation, attrIndexComb) => {
-  let result = Array.from(Array(relation.length), (x) => '');
-  for (const attrIndex of attrIndexComb) {
-    relation.forEach((row, rowIndex) => (result[rowIndex] += row[attrIndex]));
-  }
-  return result.length === [...new Set(result)].length;
+  return results; // 결과 담긴 results return
 };
-console.log(isUnique(relation, attrIndexComb));
+
+const example = [1, 2, 3, 4];
+const result = getCombinations(example, 3);
+console.log(result);
+// => [ [ 1, 2, 3 ], [ 1, 2, 4 ], [ 1, 3, 4 ], [ 2, 3, 4 ] ]
