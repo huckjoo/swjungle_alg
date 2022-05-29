9184daade6279cc09d4f0c616a608c7af71e2928
huckjoo <huckjoo96@naver.com>
2022-05-29 22:33:44 +0900

    study destruct and copy

--- test/copy.js
+++ test/copy.js
@@ -1,11 +1,10 @@
 const array = Array.from(Array(3), () => Array(3).fill(0));
-const newArr = array.map((row) => [...row]);
+const newArr = [...array];
 
 console.log(array === newArr); // false
-console.log(array[0] === newArr[0]); // false
+console.log(array[0] === newArr[0]); // true
 
 array[0][0] = 100;
 console.log(array, 'original');
-//[ [ 100, 0, 0 ], [ 0, 0, 0 ], [ 0, 0, 0 ] ] original
+
 console.log(newArr, 'new');
-//[ [ 0, 0, 0 ], [ 0, 0, 0 ], [ 0, 0, 0 ] ] new
---
+++ test/destruct.js
@@ -0,0 +1,4 @@
+const state = { a: 1, b: 2, c: 3 };
+const temp = { ...state, a: 4 };
+
+console.log(temp);
