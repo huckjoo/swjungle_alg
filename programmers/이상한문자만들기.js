b8a80a3759911574fef623453d42865bc76b93a5
huckjoo <huckjoo96@naver.com>
2022-06-06 00:03:58 +0900

    fix: 이상한 문자 만들기.js

--- programmers/이상한문자만들기.js
+++ programmers/이상한문자만들기.js
@@ -1,13 +1,19 @@
 function solution(s) {
   let answer = '';
-  let splited = s.split(' ');
-  s = splited.join('')
-  for(let i=0;i<s.length;i++){
-      if (i%2===0){
-          answer += s[i].toUpperCase();
-      }else{
-          answer += s[i]
+  let cnt = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === ' ') {
+      answer += ' ';
+      cnt = 0;
+    } else {
+      if (cnt % 2 === 0) {
+        answer += s[i].toUpperCase();
+        cnt += 1;
+      } else {
+        answer += s[i].toLowerCase();
+        cnt += 1;
       }
+    }
   }
   return answer;
 }
